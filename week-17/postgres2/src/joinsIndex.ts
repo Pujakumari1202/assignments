0373e2be654d6d99ad760a4f930bcfb56249e34d
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-04-13 21:24:15 +0530

    commit all the code of week-17 sql

--- week-17/postgres2/src/joinsIndex.ts
+++ week-17/postgres2/src/joinsIndex.ts
@@ -1,53 +1,3 @@
-// import  {Client} from "pg";
-// import express from "express";
-
-
-
-// const app=express();
-// app.use(express.json());
-
-// const pgClient3=new Client("postgresql://neondb_owner:npg_UTFGDoA7WP8H@ep-spring-union-a5n919mh-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require");
-
-// pgClient3.connect();
-
-// app.post("/signup",async(req,res)=>{
-//     const username=req.body.username;
-//     const password=req.body.password;
-//     const email=req.body.email;
-
-//     const city=req.body.city;
-//     const country=req.body.country;
-//     const street=req.body.street;
-//     const pincode=req.body.pincode;
-
-//     try{
-//         //inserted into user table
-//         const insertQuery='INSERT INTO users (username,password,email) VALUES ($1,$2,$3) RETURNING id;'
-
-//         // it somehow returns the id of that new user
-//         const response=await pgClient3.query(insertQuery,[username,password,email]);
-
-//         const userId=response.rows[0].id;
-
-//         const addresssInsertQuery='INSERT INTO addresses (city,country,street,pincode,user_id) VALUES ($1,$2,$3,$4,$5);'
-
-//         const addressInsertReponse=await pgClient3.query(addresssInsertQuery,[city,country,street,pincode,userId]);
-
-//         res.json({
-//             message:"You have signed  up"
-//         })
-//     }catch(e){
-//         console.log(e);
-//         res.json({
-//             message:"Error while signing up"
-//         })
-//     }
-
-// })
-
-
-// app.listen(3000);
-
 
 
 import {Client} from "pg";
@@ -124,16 +74,18 @@ app.get("/metadata",async(req,res)=>{
 
 })
 
+
 app.get("/better-metadata",async(req,res)=>{
     const id=req.query.id;
 
-    const query='SELECT users.id,users.username,users.email,addresses.city,addresses.country,addresses.street,addresses.pincode FROM users JOIN addresses ON users.id=addresses.user_id WHERE users.id=$1;'
+    const query='SELECT users.id,users.username,users.email,addresses.city,addresses.country,addresses.street,addresses.pincode FROM users JOIN addresses ON user.id=addresses.user_id WHERE users.id=$1;'
 
     const response =await pgClient4.query(query,[id]);
 
     res.json({
         response:response.rows
     })
-})
+});
+
 
 app.listen(3001);
