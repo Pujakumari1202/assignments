3df2a791f6a9031f8a8e70e4b3dfe63dac23e7fb
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-04-15 23:44:45 +0530

    commit all nextjs files and folder of week-18.2

---
+++ week-18.2/nextjs/app/users/page.tsx
@@ -0,0 +1,8 @@
+// this is called file based routing (we dont need to render it on page.tsx file we only have to create folder inside only need to create function no need to render it)
+export default function Home(){
+    return (
+        <div>
+            Users page
+        </div>
+    );
+}
