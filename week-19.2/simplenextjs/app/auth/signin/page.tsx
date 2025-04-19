d31e3ab7e5e7333f80723a9a46b80ef50c0d8cfc
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-04-20 01:01:32 +0530

    commit user and signin code of week-19.2

---
+++ week-19.2/simplenextjs/app/auth/signin/page.tsx
@@ -0,0 +1,16 @@
+export default function Signin(){
+    return <div>
+        Signin page
+
+        <br />
+        <br />
+
+        <input type="text" placeholder="email" />
+
+
+        <input type="password" placeholder="password" />
+
+        <button>Sign in</button>
+    </div>
+
+}
